c250a89c51f06482b8cc31201dcd4b1b6152c9e6
bon <bonuet@gmai.com>
2024-11-09 20:13:54 +0700

    fix

--- main.py
+++ main.py
@@ -121,26 +121,43 @@ def shutdown(name):
 
 def help_info():
     print("------ Main Menu ------")
-    print("1. Manage Existing VMs")
-    print("2. Custom Network")
-    print("3. Custom VM with Cloud-Init")
-    print("4. Quit Program\n")
+    print("1. Manager VMs")
+    print("2. Manager Networks")
+    print("3. Quit Program\n")
 
-def help_vm():
-    print("------ Manage Existing VMs ------")
+
+def help_manager_vms():
+    print("------ Manager VMs ------")
     print("1. List VMs and Status")
-    print("2. Start VM")
-    print("3. Shutdown VM")
-    print("4. Destroy VM")
-    print("5. Suspend VM")
-    print("6. Resume VM")
-    print("7. Create VM")
+    print("2. Create VM")
+    print("3. Start VM")
+    print("4. Suspend VM")
+    print("5. Resume VM")
+    print("6. Shutdown VM")
+    print("7. Destroy VM")
     print("8. Delete VM")
     print("9. Back")
     print("10. Quit Program\n")
 
-def help_network():
-    print("------ Custom Network ------")
+
+def help_create_vm():
+    print("------ Create VM ------")
+    print("1. Default")
+    print("2. Custom (Cloud-Init)")
+    print("3. Back")
+    print("4. Quit Program\n")
+
+
+def help_custom_cloud_init():
+    print("------ Custom VM with Cloud-Init ------")
+    print("1. Create VM with Cloud-Init")
+    print("2. Create Multiple VMs with Cloud-Init")
+    print("3. Back")
+    print("4. Quit Program\n")
+
+
+def help_manager_networks():
+    print("------ Manager Networks ------")
     print("1. List Network")
     print("2. List Bridge")
     print("3. Create Network")
@@ -151,44 +168,98 @@ def help_network():
     print("8. Back")
     print("9. Quit Program\n")
 
-def help_cloud_init():
-    print("------ Custom VM with Cloud-Init ------")
-    print("1. Create VM with Cloud-Init")
-    print("2. Create Multiple VMs with Cloud-Init")
-    print("3. Back")
-    print("4. Quit Program\n")
 
 def menu():
     while True:
         help_info()
         main_choice = input("> ")
 
-        if main_choice == "1":
+        if main_choice == "1":  # Manager VMs
             while True:
-                help_vm()
+                help_manager_vms()
                 vm_choice = input("> ")
 
-                if vm_choice in ["1", "list"]:
+                if vm_choice == "1":
                     listDomains()
-                elif vm_choice in ["2", "start"]:
+                elif vm_choice == "2":  # Create VM
+                    while True:
+                        help_create_vm()
+                        create_vm_choice = input("> ")
+
+                        if create_vm_choice == "1":  # Default create VM
+                            arg = input("Enter VM name to create: ")
+                            define(arg)
+                        elif create_vm_choice == "2":  # Custom VM with Cloud-Init
+                            while True:
+                                help_custom_cloud_init()
+                                cloud_choice = input("> ")
+
+                                if cloud_choice == "1":  # Create VM with Cloud-Init
+                                    vm_name = input("Enter VM based name: ")
+                                    ram = input_default("Enter VM ram (default = 1024): ", 1024)
+                                    vcpus = input_default("Enter VM vcpus (default = 1): ", 1)
+                                    network_name = input("Enter network name: ")
+                                    ip_address = input("Enter IP address: ")
+                                    netmask = input_default("Enter netmask (default = 255.255.255.0): ", "255.255.255.0")
+                                    gateway = input("Enter gateway: ")
+                                    portgroup = input_default("Enter VLAN (default=none): ", "none")
+                                    base_image_path = input_default(
+                                        "Enter path to base image (default: /var/lib/libvirt/images/focal-server-cloudimg-amd64.img): ",
+                                        default_value="/var/lib/libvirt/images/focal-server-cloudimg-amd64.img")
+                                    cloud_init.create_vm_with_cloud_init(conn, vm_name, network_name, ip_address, netmask,
+                                                                         gateway, base_image_path, ram, vcpus, portgroup)
+                                elif cloud_choice == "2":  # Create Multiple VMs with Cloud-Init
+                                    vm_name = input("Enter VM based name: ")
+                                    number_of_vms = int(input("Number of VMs: "))
+                                    ram = input_default("Enter VMs ram (default = 1024): ", 1024)
+                                    vcpus = input_default("Enter VMs vcpus (default = 1): ", 1)
+                                    network_name = input("Enter network name: ")
+                                    ip_range_start = input("Started IP range: ")
+                                    ip_range_end = input("Ended IP range: ")
+                                    netmask = input_default("Enter netmask (default = 255.255.255.0): ",
+                                                            "255.255.255.0")
+                                    gateway = input("Enter gateway: ")
+                                    portgroup = input_default("Enter VLAN (default=none): ", "none")
+                                    base_image_path = input_default(
+                                        "Enter path to base image (default: /var/lib/libvirt/images/focal-server-cloudimg-amd64.img): ",
+                                        default_value="/var/lib/libvirt/images/focal-server-cloudimg-amd64.img")
+                                    cloud_init.create_multiple_vms_with_cloud_init(None, vm_name, network_name,
+                                                                                   number_of_vms, ip_range_start,
+                                                                                   ip_range_end, netmask, gateway,
+                                                                                   base_image_path, ram, vcpus,
+                                                                                   portgroup)
+                                elif cloud_choice == "3":
+                                    break
+                                elif cloud_choice == "4":
+                                    print("Exiting Program.")
+                                    return
+                                else:
+                                    print("Invalid command, please try again.\n")
+
+                        elif create_vm_choice == "3":
+                            break
+                        elif create_vm_choice == "4":
+                            print("Exiting Program.")
+                            return
+                        else:
+                            print("Invalid command, please try again.\n")
+
+                elif vm_choice == "3":
                     arg = input("Enter VM name to start: ")
                     start(arg)
-                elif vm_choice in ["3", "shutdown"]:
-                    arg = input("Enter VM name to shutdown: ")
-                    shutdown(arg)
-                elif vm_choice in ["4", "destroy"]:
-                    arg = input("Enter VM name to destroy: ")
-                    destroy(arg)
-                elif vm_choice in ["5", "suspend"]:
+                elif vm_choice == "4":
                     arg = input("Enter VM name to suspend: ")
                     suspend(arg)
-                elif vm_choice in ["6", "resume"]:
+                elif vm_choice == "5":
                     arg = input("Enter VM name to resume: ")
                     resume(arg)
-                elif vm_choice in ["7", "create"]:
-                    arg = input("Enter VM name to create: ")
-                    define(arg)
-                elif vm_choice in ["8", "delete"]:
+                elif vm_choice == "6":
+                    arg = input("Enter VM name to shutdown: ")
+                    shutdown(arg)
+                elif vm_choice == "7":
+                    arg = input("Enter VM name to destroy: ")
+                    destroy(arg)
+                elif vm_choice == "8":
                     arg = input("Enter VM name to delete: ")
                     undefine(arg)
                 elif vm_choice == "9":
@@ -199,9 +270,9 @@ def menu():
                 else:
                     print("Invalid command, please try again.\n")
 
-        elif main_choice == "2":
+        elif main_choice == "2":  # Manager Networks
             while True:
-                help_network()
+                help_manager_networks()
                 network_choice = input("> ")
                 if network_choice in ["1", "list_network"]:
                     list_network()
@@ -219,7 +290,7 @@ def menu():
                 elif network_choice in ["6", "delete_bridge"]:
                     arg = input("Enter Bridge name to delete: ")
                     delete_br(arg)
-                elif network_choice in ["7", "connect_trunk"]:
+                elif network_choice == "7":
                     bridge1 = input("Enter first bridge name: ")
                     bridge2 = input("Enter second bridge name: ")
                     trunk(bridge1, bridge2)
@@ -232,53 +303,11 @@ def menu():
                     print("Invalid command, please try again.\n")
 
         elif main_choice == "3":
-            while True:
-                help_cloud_init()
-                cloud_choice = input("> ")
-
-                if cloud_choice == "1":
-                    vm_name = input("Enter VM based name: ")
-                    ram = input_default("Enter VM ram (default = 1024): ", 1024)
-                    vcpus = input_default("Enter VM vcpus (default = 1): ", 1)
-                    network_name = input("Enter network name: ")
-                    ip_address = input("Enter IP address: ")
-                    netmask = input_default("Enter netmask (default = 255.255.255.0): ", "255.255.255.0")
-                    gateway = input("Enter gateway: ")
-                    portgroup = input_default("Enter VLAN (default=none): ", "none")
-                    base_image_path = input_default("Enter path to base image (default: /var/lib/libvirt/images/focal-server-cloudimg-amd64.img): ",
-                                                    default_value="/var/lib/libvirt/images/focal-server-cloudimg-amd64.img")
-                    cloud_init.create_vm_with_cloud_init(conn, vm_name, network_name, ip_address, netmask, gateway,
-                                                         base_image_path, ram, vcpus, portgroup)
-                elif cloud_choice == "2":
-                    vm_name = input("Enter VM based name: ")
-                    number_of_vms = int(input("Number of VMs: "))
-                    ram = input_default("Enter VMs ram (default = 1024): ", 1024)
-                    vcpus = input_default("Enter VMs vcpus (default = 1): ", 1)
-                    network_name = input("Enter network name: ")
-                    ip_range_start = input("Started IP range: ")
-                    ip_range_end = input("Ended IP range: ")
-                    netmask = input_default("Enter netmask (default = 255.255.255.0): ", "255.255.255.0")
-                    gateway = input("Enter gateway: ")
-                    portgroup = input_default("Enter VLAN (default=none): ", "none")
-                    base_image_path = input_default("Enter path to base image (default: /var/lib/libvirt/images/focal-server-cloudimg-amd64.img): ",
-                                                    default_value="/var/lib/libvirt/images/focal-server-cloudimg-amd64.img")
-                    cloud_init.create_multiple_vms_with_cloud_init(None, vm_name, network_name, number_of_vms,
-                                                                   ip_range_start, ip_range_end, netmask, gateway,
-                                                                   base_image_path, ram, vcpus, portgroup)
-                elif cloud_choice == "3":
-                    break
-                elif cloud_choice == "4":
-                    print("Exiting Program.")
-                    return
-                else:
-                    print("Invalid command, please try again.\n")
-
-        elif main_choice == "4":
             print("Exiting Program.")
             break
-
         else:
             print("Invalid command, please try again.\n")
+
 
 
 if __name__ == "__main__":
